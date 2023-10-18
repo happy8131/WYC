daf3c110e4f093911e781daae82e761ac45e3449
happy8131 <lion8131@naver.com>
2023-10-19 01:10:37 +0900

    img add , img init

--- components/header.tsx
+++ components/header.tsx
@@ -3,10 +3,17 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import Swal from "sweetalert2";
 
+import styled from "styled-components";
+
+export const HeaderImg = styled.img`
+  width: 20%;
+`;
+
 const Header = () => {
   const router = useRouter();
   const user = auth.currentUser;
   const nav = router.asPath;
+
   const onLogOut = async () => {
     Swal.fire({
       icon: "question",
@@ -46,19 +53,7 @@ const Header = () => {
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link href="/">
             <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-              </svg>
-              <span className="ml-3 text-xl">With You Camping</span>
+              <HeaderImg src="/logo5.jpg" />
             </a>
           </Link>
           <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
--- components/myPost.tsx
+++ components/myPost.tsx
@@ -96,15 +96,7 @@ export default function MyPost({
           {Boolean(avatarPhoto) ? (
             <AvatarImg src={avatarPhoto as string} />
           ) : (
-            <svg
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6 text-white p-2 bg-indigo-500 rounded-full"
-              aria-hidden="true"
-            >
-              <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
-            </svg>
+            <AvatarImg src="/logo3.jpg" />
           )}
           {user?.displayName}
         </div>
